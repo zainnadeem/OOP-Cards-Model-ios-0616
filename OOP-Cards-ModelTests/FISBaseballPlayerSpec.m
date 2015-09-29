d7dda6e55f0a22d04d02285ad6147143fab7078b
TOMalley104 <tom@flatironschool.com>
2015-09-29 15:36:57 -0400

    clean up tests

--- OOP-Cards-ModelTests/FISBaseballPlayerSpec.m
+++ OOP-Cards-ModelTests/FISBaseballPlayerSpec.m
@@ -20,56 +20,34 @@ describe(@"FISBaseballPlayer", ^{
         player = [[FISBaseballPlayer alloc] initWithFirstName:@"Hank" lastName:@"Aaron" weight:180.0f number:44 height:72.0f];
     });
     
-    describe(@"default initializer", ^{
+    describe(@"default initializer sets", ^{
         __block FISBaseballPlayer *defaultPlayer;
         
         beforeAll(^{
             defaultPlayer = [[FISBaseballPlayer alloc] init];
         });
         
-        it(@"should return the default weight",^{
-            expect(defaultPlayer.weight).to.equal(0);
+        it(@"weight to 0.0",^{
+            expect(defaultPlayer.weight).to.equal(0.0);
         });
         
-        it(@"should return the default height",^{
-            expect(defaultPlayer.height).to.equal(0);
+        it(@"height to 0.0",^{
+            expect(defaultPlayer.height).to.equal(0.0);
         });
         
-        it(@"should return the default number",^{
+        it(@"number to 0",^{
             expect(defaultPlayer.number).to.equal(0);
         });
         
-        it(@"should return the default first name",^{
+        it(@"firstName to empty string",^{
             expect(defaultPlayer.firstName).to.equal(@"");
         });
         
-        it(@"should return the default last name",^{
+        it(@"lastName to empty string",^{
             expect(defaultPlayer.lastName).to.equal(@"");
         });
     });
     
-    describe(@"baseball player initializer", ^{
-        
-        it(@"should set weight to the correct value",^{
-            expect(player.weight).to.equal(180);
-        });
-        
-        it(@"should set height to the correct value",^{
-            expect(player.height).to.equal(72);
-        });
-        
-        it(@"should set number to the correct value",^{
-            expect(player.number).to.equal(44);
-        });
-        
-        it(@"should set first name to the correct value",^{
-            expect(player.firstName).to.equal(@"Hank");
-        });
-        
-        it(@"should set last name to the correct value",^{
-            expect(player.lastName).to.equal(@"Aaron");
-        });
-    });
     
     describe(@"convertLbsToKGsWithLbs", ^{
         it(@"should perform a weight conversion and return the expected value",^{
@@ -77,43 +55,6 @@ describe(@"FISBaseballPlayer", ^{
             expect(kilos).to.equal(82);
         });
     });
-    
-#pragma mark - Property setter tests
-    
-    describe(@"weight property", ^{
-        it(@"weight should be what I set it to",^{
-            player.weight = 250;
-            expect(player.weight).to.equal(250);
-        });
-    });
-    
-    describe(@"number property", ^{
-        it(@"number should be what I set it to",^{
-            player.number = 55;
-            expect(player.number).to.equal(55);
-        });
-     
-    describe(@"height property", ^{
-        it(@"height should be what I set it to",^{
-            player.height = 72;
-            expect(player.height).to.equal(72);
-        });
-    });
-        
-    describe(@"first name property", ^{
-        it(@"first name should be what I set it to",^{
-            player.firstName = @"Al";
-            expect(player.firstName).to.equal(@"Al");
-        });
-    });
-        
-    describe(@"last name property", ^{
-        it(@"last name should be what I set it to",^{
-            player.lastName = @"Tyus";
-            expect(player.lastName).to.equal(@"Tyus");
-        });
-    });   });
-    
 });
 
 SpecEnd
--- OOP-Cards-ModelTests/FISPlayingCardSpec.m
+++ OOP-Cards-ModelTests/FISPlayingCardSpec.m
@@ -19,113 +19,50 @@ describe(@"FISPlayingCard", ^{
         myCard = [[FISPlayingCard alloc] initWithSuit:@"♠" rank:5];
     });
     
-    it(@"exists", ^{
-        expect(myCard).notTo.beNil();
-    });  
-    
-    describe(@"initWithSuit:Rank:", ^{
-        it(@"should have a suit matching what I set it to",^{
-            expect(myCard.suit).to.equal(@"♠");
-        });
-        
-        it(@"should have a rank matching what I set it to",^{
-            expect(myCard.rank).to.equal(5);
-        });
-    });
-    
-    describe(@"init", ^{
+    describe(@"default init sets", ^{
         __block FISPlayingCard *defaultCard = [[FISPlayingCard alloc] init];
-        it(@"should set suit to an empty string by default",^{
+        it(@"suit to empty string",^{
             expect(defaultCard.suit).to.equal(@"");
         });
         
-        it(@"should set rank to 0 by default",^{
+        it(@"rank to 0",^{
             expect(defaultCard.rank).to.equal(0);
         });
     });
     
-    describe(@"setSuit", ^{
-        it(@"the suit should match the value I set",^{
-            myCard.suit = @"♥";
-            expect(myCard.suit).to.equal(@"♥");
-        });
-    });
-    
-    describe(@"setRank", ^{
-        it(@"the rank should match the value I set",^{
-            myCard.rank = 10;
-            expect(myCard.rank).to.equal(10);
-        });
-    });
-    
-    describe(@"valid rank with designated initializer", ^{
+    describe(@"rank validation", ^{
+        __block FISPlayingCard *aPlayingCard;
         
-        FISPlayingCard *myValidPlayingCard = [[FISPlayingCard alloc] initWithSuit:@"♥" rank:13];
-        FISPlayingCard *myInvalidPlayingCard = [[FISPlayingCard alloc] initWithSuit:@"♥" rank:14];
+        beforeAll(^{
+            aPlayingCard = [[FISPlayingCard alloc] init];
+        });
+        
+        it(@"returns valid ranks",^{
+            aPlayingCard.rank = 5;
+            expect(aPlayingCard.rank).to.equal(5);
+        });
+        
+        it(@"return 0 for invalid ranks",^{
+            aPlayingCard.rank = 15;
+            expect(aPlayingCard.rank).to.equal(0);
+        });
+    });
 
-        it(@"valid ranks should return the value as an NSNumber",^{
-            expect(myValidPlayingCard.rank).to.equal(13);
-        });
-        
-        it(@"invalid ranks should return 0 value",^{
-            expect(myInvalidPlayingCard.rank).to.equal(0);
-        });
-    });
-    
-    describe(@"valid rank with setter", ^{
-        __block FISPlayingCard *myValidPlayingCard;
-        __block FISPlayingCard *myInvalidPlayingCard;
+    describe(@"suit validation", ^{
+        __block FISPlayingCard *aPlayingCard;
         
         beforeAll(^{
-            myValidPlayingCard = [[FISPlayingCard alloc] init];
-            myInvalidPlayingCard = [[FISPlayingCard alloc] init];
-            myValidPlayingCard.rank = 13;
-            myInvalidPlayingCard.rank = 14;
+            aPlayingCard = [[FISPlayingCard alloc] init];
         });
         
-        it(@"valid ranks should return value as NSNumber",^{
-            expect(myValidPlayingCard.rank).to.equal(13);
+        it(@"returns a valid suit",^{
+            aPlayingCard.suit = @"♥";
+            expect(aPlayingCard.suit).to.equal(@"♥");
         });
         
-        it(@"invalid ranks should return 0 value",^{
-            expect(myInvalidPlayingCard.rank).to.equal(0);
-        });
-    });
-    
-    describe(@"valid suit with with designated initializer", ^{
-        __block FISPlayingCard *myValidPlayingCard;
-        __block FISPlayingCard *myInvalidPlayingCard;
-        
-        beforeAll(^{
-            myValidPlayingCard = [[FISPlayingCard alloc] initWithSuit:@"♥" rank:13];
-            myInvalidPlayingCard = [[FISPlayingCard alloc] initWithSuit:@"X" rank:14];
-        });
-        
-        it(@"valid suits should return suit",^{
-            expect(myValidPlayingCard.suit).to.equal(@"♥");
-        });
-        it(@"invalid suits should return an empty string",^{
-            expect(myInvalidPlayingCard.suit).to.equal(@"");
-        });
-    });
-    
-    describe(@"valid suit with setter", ^{
-        __block FISPlayingCard *myValidPlayingCard;
-        __block FISPlayingCard *myInvalidPlayingCard;
-        
-        beforeAll(^{
-            myValidPlayingCard = [[FISPlayingCard alloc] init];
-            myInvalidPlayingCard = [[FISPlayingCard alloc] init];
-        });
-        
-        it(@"valid suits should return a valid suit",^{
-            myValidPlayingCard.suit = @"♥";
-            expect(myValidPlayingCard.suit).to.equal(@"♥");
-        });
-        
-        it(@"invalid suits should return an empty string",^{
-            myInvalidPlayingCard.suit = @"X";
-            expect(myInvalidPlayingCard.suit).to.equal(@"");
+        it(@"return an empty string for invalid suit",^{
+            aPlayingCard.suit = @"X";
+            expect(aPlayingCard.suit).to.equal(@"");
         });
     });
 });
