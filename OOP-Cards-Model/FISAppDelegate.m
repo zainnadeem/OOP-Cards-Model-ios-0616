121dc84a585fd53ce351e09c7c3cd18d432a31e0
markedwardmurray <markedwardmurray@gmail.com>
2015-10-01 18:08:30 -0400

    replace solution

--- OOP-Cards-Model/FISAppDelegate.m
+++ OOP-Cards-Model/FISAppDelegate.m
@@ -1,11 +1,24 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISCard.h"
+#import "FISCardDeck.h"
 
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FISCard *card = [[FISCard alloc] init];
+    NSLog(@"%@", card.description);
+    
+    FISCardDeck *deck = [[FISCardDeck alloc] init];
+    NSLog(@"%@", deck.description);
+    
+    [deck resetDeck];
+    NSLog(@"%@", deck.description);
+    
+    
     return YES;
 }
 
